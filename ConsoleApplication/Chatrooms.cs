f428674f1924322663fb7a522f57091a5acbf440
Arnaud M <arnaud.marchal.20160574@efrei.net>
2017-12-15 09:28:04 +0100

    Creating Chatrooms.cs.
    La class Chatrooms est devenue plus longue et plus importante et
    mérite donc désormais d'être dans son propre fichier (et cela allège
    également Server.cs par la même occasion).

---
+++ ConsoleApplication/Chatrooms.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace super_chainsaw_sharpChatClient
+{
+    public class Chatrooms : List<Chatroom>
+    {
+        public delegate void manageChatrooms(Chatroom chatroom);
+        public event manageChatrooms ChatroomAdded;
+
+        public new void Add(Chatroom chatroom)
+        {
+            base.Add(chatroom);
+            ChatroomAdded(chatroom);
+        }
+
+        public List<string> names()
+        {
+            List<string> chatroomsNames = new List<string>();
+            foreach (var chatroom in this)
+                chatroomsNames.Add(chatroom.name);
+
+            return chatroomsNames;
+        }
+
+        public Chatroom fromName(string chatroomString)
+        {
+            foreach (var chatroom in this)
+                if (chatroom.name == chatroomString)
+                    return chatroom;
+
+            return null;
+        }
+    }
+}
